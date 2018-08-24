4c7dedf0eee8cf5e8b59f851c36690d1a843db13
voxqhuy <voxqhuy@gmail.com>
2018-08-24 16:08:57 -0400

    the website is basically looking good

---
+++ javascript.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+        // open or close navbar
+        $('#sidebar, #content').toggleClass('active');
+    });
+})
