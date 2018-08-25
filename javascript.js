c71fa9eefa9c1f0fe346384132058d2d405b4f22
kohnewlife <voxqhuy@gmail.com>
2018-08-25 01:03:39 -0400

    added time conversion

--- javascript.js
+++ javascript.js
@@ -7,4 +7,18 @@ $(document).ready(function() {
         // open or close navbar
         $('#sidebar, #content').toggleClass('active');
     });
+
+    $('#timeToConvert').change(function() {
+    	let time = $(this).val();
+    	let hours = time.split(":")[0];
+    	let minutes = time.split(":")[1];
+    	var decimalTime = hours * 1.0 + minutes * 1.0 / 60;
+    	$('#decimalTime').val(decimalTime.toFixed(2));
+
+    });
+
+    $('#currentHours').change(function() {
+    	let a = 1;
+    	alert($('#currentHours').val());
+    });
 })
